7940567cec0976301c471b64bae281a1e32d8d8d
Sylvain Tshiasuma Lys <sylvain.tshiasuma2017@gmail.com>
2022-10-26 11:22:51 +0100

    ADD node routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose")
+const UserModel = require('./models/Users')
 
+mongoose.connect("mongodb+srv://sylvain:dipro0243@cluster1.gwadefh.mongodb.net/mern-stack?retryWrites=true&w=majority")
+
+app.get("/getUsers", (req, res) => {
+    UserModel.find({}, (err, result) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(result);
+      }
+    });
+  });
+// app.post()
 app.listen(3001, () => {
     console.log('Premier test réussi parfectement');
 })
